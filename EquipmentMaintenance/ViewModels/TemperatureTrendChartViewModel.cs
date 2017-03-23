83ed6d2a368883e415da295edbb97dd9f752605d
Minh Nguyen <minh.nguyen@tpssoft.com>
2017-03-23 10:11:53 +0700

    chart

--- EquipmentMaintenance/ViewModels/TemperatureTrendChartViewModel.cs
+++ EquipmentMaintenance/ViewModels/TemperatureTrendChartViewModel.cs
@@ -8,8 +8,10 @@ namespace EquipmentMaintenance.ViewModels
 {
     public class TemperatureTrendChartViewModel : BindableBase
     {
+        private DateTime _date;
         public TemperatureTrendChartViewModel()
         {
+            _date = new DateTime(2017, 1, 1, 11, 0, 0);
             ChartMapper();
         }
 
@@ -22,6 +24,7 @@ namespace EquipmentMaintenance.ViewModels
             //lets save the mapper globally.
             Charting.For<MeasureModel>(mapper);
         }
+        
 
         //the values property will store our values array
         private ChartValues<MeasureModel> _series1;
@@ -32,25 +35,25 @@ namespace EquipmentMaintenance.ViewModels
                 if (_series1 == null)
                 {
                     _series1 = new ChartValues<MeasureModel> {
-                        new MeasureModel { DateTime = DateTime.Now, Value = 20000 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 20010 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 20100 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 19832 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 19820 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 22000 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 19300 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 20000 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 18300 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 23030 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 20000 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 20000 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 20000 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 42400 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 20000 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 20000 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 20000 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 20000 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 20000 }
+                        new MeasureModel { DateTime = _date.AddMinutes(000), Value = 20000 },
+                        new MeasureModel { DateTime = _date.AddMinutes(010), Value = 20010 },
+                        new MeasureModel { DateTime = _date.AddMinutes(020), Value = 20100 },
+                        new MeasureModel { DateTime = _date.AddMinutes(030), Value = 19832 },
+                        new MeasureModel { DateTime = _date.AddMinutes(040), Value = 19820 },
+                        new MeasureModel { DateTime = _date.AddMinutes(050), Value = 22000 },
+                        new MeasureModel { DateTime = _date.AddMinutes(060), Value = 19300 },
+                        new MeasureModel { DateTime = _date.AddMinutes(070), Value = 20000 },
+                        new MeasureModel { DateTime = _date.AddMinutes(080), Value = 18300 },
+                        new MeasureModel { DateTime = _date.AddMinutes(090), Value = 23030 },
+                        new MeasureModel { DateTime = _date.AddMinutes(100), Value = 20000 },
+                        new MeasureModel { DateTime = _date.AddMinutes(110), Value = 20000 },
+                        new MeasureModel { DateTime = _date.AddMinutes(120), Value = 20000 },
+                        new MeasureModel { DateTime = _date.AddMinutes(130), Value = 42400 },
+                        new MeasureModel { DateTime = _date.AddMinutes(140), Value = 20000 },
+                        new MeasureModel { DateTime = _date.AddMinutes(150), Value = 20000 },
+                        new MeasureModel { DateTime = _date.AddMinutes(160), Value = 20000 },
+                        new MeasureModel { DateTime = _date.AddMinutes(170), Value = 20000 },
+                        new MeasureModel { DateTime = _date.AddMinutes(180), Value = 20000 }
                     };
                 }
                 return _series1;
@@ -68,25 +71,25 @@ namespace EquipmentMaintenance.ViewModels
                 if (_series2 == null)
                 {
                     _series2 = new ChartValues<MeasureModel> {
-                        new MeasureModel { DateTime = DateTime.Now, Value = 15000 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 15500 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 15100 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 15300 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 15200 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 14000 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 14300 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 15000 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 15200 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 14900 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 15000 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 15000 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 14900 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 15100 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 15000 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 26000 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 18500 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 15000 },
-                        new MeasureModel { DateTime = DateTime.Now, Value = 15000 }
+                        new MeasureModel { DateTime = _date.AddMinutes(000), Value = 15000 },
+                        new MeasureModel { DateTime = _date.AddMinutes(010), Value = 15500 },
+                        new MeasureModel { DateTime = _date.AddMinutes(020), Value = 15100 },
+                        new MeasureModel { DateTime = _date.AddMinutes(030), Value = 15300 },
+                        new MeasureModel { DateTime = _date.AddMinutes(040), Value = 15200 },
+                        new MeasureModel { DateTime = _date.AddMinutes(050), Value = 14000 },
+                        new MeasureModel { DateTime = _date.AddMinutes(060), Value = 14300 },
+                        new MeasureModel { DateTime = _date.AddMinutes(070), Value = 15000 },
+                        new MeasureModel { DateTime = _date.AddMinutes(080), Value = 15200 },
+                        new MeasureModel { DateTime = _date.AddMinutes(090), Value = 14900 },
+                        new MeasureModel { DateTime = _date.AddMinutes(100), Value = 15000 },
+                        new MeasureModel { DateTime = _date.AddMinutes(110), Value = 15000 },
+                        new MeasureModel { DateTime = _date.AddMinutes(120), Value = 14900 },
+                        new MeasureModel { DateTime = _date.AddMinutes(130), Value = 15100 },
+                        new MeasureModel { DateTime = _date.AddMinutes(140), Value = 15000 },
+                        new MeasureModel { DateTime = _date.AddMinutes(150), Value = 26000 },
+                        new MeasureModel { DateTime = _date.AddMinutes(160), Value = 18500 },
+                        new MeasureModel { DateTime = _date.AddMinutes(170), Value = 15000 },
+                        new MeasureModel { DateTime = _date.AddMinutes(180), Value = 15000 }
                     };
                 }
                 return _series2;

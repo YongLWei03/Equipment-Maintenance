d4b5a6cb939e28b3ba22301b9ab62874705b80d0
iotlo <iotlo@DESKTOP-VBI5RQH>
2017-03-28 00:06:09 +0700

    設備保全管理システム

--- EquipmentMaintenance/Properties/AssemblyInfo.cs
+++ EquipmentMaintenance/Properties/AssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EquipmentMaintenance")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("設備保全管理システム")]
+[assembly: AssemblyDescription("設備保全管理システム")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EquipmentMaintenance")]

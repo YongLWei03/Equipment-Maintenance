e97594961702261b3025ddfc27db50221a6b1b7a
Minh Nguyen <iot.localhost@gmail.com>
2017-03-28 14:50:19 +0700

    check setup

--- EquipmentMaintenance/Properties/AssemblyInfo.cs
+++ EquipmentMaintenance/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("設備保全管理システム")]
-[assembly: AssemblyDescription("設備保全管理システム")]
+[assembly: AssemblyTitle("Equipment Maintenance")]
+[assembly: AssemblyDescription("Equipment Maintenance")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EquipmentMaintenance")]
+[assembly: AssemblyProduct("Equipment Maintenance")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
